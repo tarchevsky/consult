9c76e9fae0c366053c19506cedfe98629e002440
Иван <i.tarchevsky@gmail.com>
2023-04-18 16:21:22 +0300

    Все последние изменения

--- js/swiper.js
+++ js/swiper.js
@@ -1,51 +1,50 @@
 var swiper = new Swiper('.swiper-container', {
-  // Default parameters
   slidesPerView: 4,
   spaceBetween: 0,
   slidesPerGroup: 4,
   loop: true,
   loopFillGroupWithBlank: true,
   pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+    el: '.swiper-pagination',
+    clickable: true,
   },
   navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
   breakpoints: {
-      320: {
-          slidesPerView: 1,
-          spaceBetween: 0,
-          slidesPerGroup: 1,
-      },
-      // when window width is >= 480px
-      480: {
-        slidesPerView: 1,
-        spaceBetween: 0,
-        slidesPerGroup: 1
-      },
-      // when window width is >= 640px
-      640: {
-        slidesPerView: 3,
-        spaceBetween: 0,
-        slidesPerGroup: 3
-      },
-      1280: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      slidesPerGroup: 1,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      slidesPerGroup: 1
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 0,
+      slidesPerGroup: 3
+    },
+    1280: {
       slidesPerView: 4,
       spaceBetween: 0,
       slidesPerGroup: 4,
       loop: true,
       loopFillGroupWithBlank: true,
       pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
+        el: '.swiper-pagination',
+        clickable: true,
       },
       navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
       },
-      }
+    }
   }
 });
 
@@ -57,45 +56,45 @@ var swiper = new Swiper('.swiper-2', {
   loop: true,
   loopFillGroupWithBlank: true,
   pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+    el: '.swiper-pagination',
+    clickable: true,
   },
   navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
   breakpoints: {
-      320: {
-          slidesPerView: 1,
-          spaceBetween: 0,
-          slidesPerGroup: 1,
-      },
-      // when window width is >= 480px
-      480: {
-        slidesPerView: 1,
-        spaceBetween: 0,
-        slidesPerGroup: 1
-      },
-      // when window width is >= 640px
-      640: {
-        slidesPerView: 3,
-        spaceBetween: 0,
-        slidesPerGroup: 3
-      },
-      1280: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      slidesPerGroup: 1,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      slidesPerGroup: 1
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 0,
+      slidesPerGroup: 3
+    },
+    1280: {
       slidesPerView: 4,
       spaceBetween: 0,
       slidesPerGroup: 4,
       loop: true,
       loopFillGroupWithBlank: true,
       pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
+        el: '.swiper-pagination',
+        clickable: true,
       },
       navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
       },
-      }
+    }
   }
 });
